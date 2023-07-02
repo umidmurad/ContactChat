b9949bd6a6b955728cf68c273aaadaa4e5f95d3f
Umid Muradli <umid.murad@hotmail.com>
2023-07-02 16:55:03 -0700

    Using AJAX to recieve Response

--- main.js
+++ main.js
@@ -1,8 +1,23 @@
-var message = document.getElementById("text").value;
-//When user clicks the button, get message and append it to the text variable
-document.getElementById("button").addEventListener("click", function () {
-  var message = document.getElementById("message").value;
-  var textElement = document.getElementById("text");
-  textElement.innerHTML += "<br>" + message;
-  document.getElementById("message").value = "";
-});
+document
+  .getElementById("contact-form")
+  .addEventListener("submit", function (event) {
+    event.preventDefault(); // Prevent the form from submitting normally
+
+    var formData = new FormData(this); // Get the form data
+
+    var xhr = new XMLHttpRequest(); // Create a new XMLHttpRequest object
+    xhr.open("POST", "https://umiddev.000webhostapp.com/contact.php", true); // Replace with the actual URL of your PHP script
+    xhr.onreadystatechange = function () {
+      if (xhr.readyState === XMLHttpRequest.DONE) {
+        if (xhr.status === 200) {
+          // Request successful
+          var response = xhr.responseText; // Get the response from the PHP script
+          alert(response); // Display the response message
+        } else {
+          // Request failed
+          alert("There was an error attempting to send your information.");
+        }
+      }
+    };
+    xhr.send(formData); // Send the form data to the PHP script
+  });
