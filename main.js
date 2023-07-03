a5e374f4e4fb11e6956c7a180780bf52a2178fa9
Umid Muradli <umid.murad@hotmail.com>
2023-07-03 13:57:59 -0700

    Redirection to Home Page

--- main.js
+++ main.js
@@ -1,28 +1,3 @@
-// document
-//   .getElementById("contact-form")
-//   .addEventListener("submit", function (event) {
-//     event.preventDefault();
-
-//     var formData = new FormData(this); // Get the form data
-
-//     var xhr = new XMLHttpRequest(); // Create a new XMLHttpRequest object
-//     xhr.open("POST", "https://umiddev.000webhostapp.com/contact.php", true);
-//     xhr.onreadystatechange = function () {
-//       if (xhr.readyState === XMLHttpRequest.DONE) {
-//         if (xhr.status === 200) {
-//           // Request successful
-//           var response = xhr.responseText; // Get the response from the PHP script
-//           document.getElementById("response").textContent = response; // Update the content of the element with id="response"
-//         } else {
-//           // Request failed
-//           document.getElementById("response").textContent =
-//             "There was an error attempting to send your information.";
-//         }
-//       }
-//     };
-//     xhr.send(formData); // Send the form data to the PHP script
-//   });
-
 document
   .getElementById("contact-form")
   .addEventListener("submit", function (event) {
@@ -49,3 +24,8 @@ document
     };
     xhr.send(formData); // Send the form data to the PHP script
   });
+
+function redirectToPage() {
+  // Redirect to the desired page
+  window.location.href = "https://umid.dev/"; // Replace with your desired URL
+}
